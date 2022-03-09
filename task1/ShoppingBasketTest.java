a9f7bb6424b3dcd5d6e208d878c3c9750f5c6cf5
hgoo9099 <hgoo9099@uni.sydney.edu.au>
2022-03-09 21:19:58 +1100

    all methods covered

--- task1/ShoppingBasketTest.java
+++ task1/ShoppingBasketTest.java
@@ -1,5 +1,6 @@
-Spackage task1;
+package task1;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,73 +11,144 @@ class ShoppingBasketTest{
     ShoppingBasket sb;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         this.sb = new ShoppingBasket();
     }
 
     //Constructor specific tests
     @Test
-    public void initNotNull(){
+    public void initNotNull() {
         assertNotNull(this.sb);
     }
 
     //addItem() tests
     @Test
-    public void incorrectName(){
+    public void incorrectName() {
         assertThrows(IllegalArgumentException.class, () -> this.sb.addItem("some other fruit", 1));
     }
 
     @Test
-    public void incorrectNumberAdd1(){
+    public void incorrectNumberAdd1() {
         assertThrows(IllegalArgumentException.class, () -> this.sb.addItem("apple", 0));
     }
 
     @Test
-    public void incorrectNumberAdd2(){
+    public void incorrectNumberAdd2() {
         assertThrows(IllegalArgumentException.class, () -> this.sb.addItem("apple", -1));
     }
 
     //removeItem() tests
     @Test
-    public void correctRemove(){
+    public void correctRemove() {
         this.sb.addItem("apple", 1);
         assertTrue(this.sb.removeItem("apple", 1));
     }
 
     @Test
-    public void itemNotInBasketRemove(){
+    public void itemNotInBasketRemove1() {
         assertFalse(this.sb.removeItem("apple", 1));
     }
 
     @Test
-    public void itemNotInBasketRemove(){
+    public void itemNotInBasketRemove2() {
         assertFalse(this.sb.removeItem("some other fruit", 1));
     }
 
     @Test
-    public void moreThanInBasketRemove(){
+    public void moreThanInBasketRemove() {
         this.sb.addItem("apple", 1);
         assertFalse(this.sb.removeItem("apple", 2));
     }
 
     @Test
-    public void incorrectNumberRemove1(){
+    public void incorrectNumberRemove1() {
         this.sb.addItem("apple", 1);//Don't know if it will check name or number first, so have to add first
         assertThrows(IllegalArgumentException.class, () -> this.sb.removeItem("apple", 0));
     }
 
     @Test
-    public void incorrectNumberRemove2(){
+    public void incorrectNumberRemove2() {
         this.sb.addItem("apple", 1);
         assertThrows(IllegalArgumentException.class, () -> this.sb.removeItem("apple", -1));
     }
 
     //getItems() tests
     @Test
-    public void correctGetItems(){
+    public void correctGetItems() {
         this.sb.addItem("apple", 1);
         List<javafx.util.Pair<String,Integer>> items = this.sb.getItems();
-        assertEquals(items[0].getKey(), "apple");
-        assertEquals(items[0].getValue(), 1);
+        assertEquals(items.get(0).getKey(), "apple");
+        assertEquals(items.get(0).getValue(), 1);
+    }
+
+    @Test
+    public void correctGetItemsMulti() {
+        this.sb.addItem("apple", 1);
+        this.sb.addItem("apple", 1);
+        List<javafx.util.Pair<String,Integer>> items = this.sb.getItems();
+        assertEquals(items.get(0).getKey(), "apple");
+        assertEquals(items.get(0).getValue(), 2);
+    }
+
+    @Test
+    public void correctGetItemsAddRem() {
+        this.sb.addItem("apple", 2);
+        this.sb.removeItem("apple", 1);
+        List<javafx.util.Pair<String,Integer>> items = this.sb.getItems();
+        assertEquals(items.get(0).getKey(), "apple");
+        assertEquals(items.get(0).getValue(), 1);
+    }
+
+    //getValue() tests
+    @Test
+    public void correctGetValue1() {
+        this.sb.addItem("apple", 1);
+        assertEquals(2.5, this.sb.getValue());
+    }
+
+    @Test
+    public void correctGetValue2() {
+        this.sb.addItem("orange", 1);
+        assertEquals(1.25, this.sb.getValue());
+    }
+
+    @Test
+    public void correctGetValue3() {
+        this.sb.addItem("pear", 1);
+        assertEquals(3.0, this.sb.getValue());
+    }
+
+    @Test
+    public void correctGetValue4() {
+        this.sb.addItem("banana", 1);
+        assertEquals(4.95, this.sb.getValue());
+    }
+
+    @Test
+    public void nullGetValue1() {
+        assertNull(this.sb.getValue());
+    }
+
+    @Test
+    public void nullGetValue2() {
+        this.sb.addItem("apple", 1);
+        this.sb.removeItem("apple", 1);
+        assertNull(this.sb.getValue());
+    }
+
+    //clear() tests
+    @Test
+    public void correctClear() {
+        this.sb.addItem("apple", 1);
+        this.sb.clear();
+        assertEquals(0, this.sb.getValue());
+    }
+
+    @Test
+    public void correctClearAddRem() {
+        this.sb.addItem("apple", 1);
+        this.sb.removeItem("apple", 1);
+        this.sb.clear();
+        assertEquals(null, this.sb.getValue());
     }
 }
