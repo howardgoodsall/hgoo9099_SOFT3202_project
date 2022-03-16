01c05e6c6e973ed1674517876d34a24c22e616cf
hgoo9099 <hgoo9099@uni.sydney.edu.au>
2022-03-16 21:33:43 +1100

    First commit for testing assignment, login, logout and injectAuth tests somewhat done

---
+++ testingassignment/BSFacadeImplTest.java
@@ -0,0 +1,102 @@
+package au.edu.sydney.soft3202.reynholm.erp.billingsystem;
+
+import au.edu.sydney.soft3202.reynholm.erp.cheatmodule;
+import au.edu.sydney.soft3202.reynholm.erp.client;
+import au.edu.sydney.soft3202.reynholm.erp.compliance;
+import au.edu.sydney.soft3202.reynholm.erp.permissions;
+import au.edu.sydney.soft3202.reynholm.erp.project;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BSFacadeImplTest {
+    BSFacadeImpl bsfi;
+    ERPCheatFactory cheatFact;
+    AuthenticationModule authenMod;
+    AuthorisationModule authorMod;
+
+    @BeforeEach
+    public void setUp() {
+        this.bsfi  = new BSFacadeImpl();
+        this.cheatFact = new ERPCheatFactory();
+        this.authenMod = this.cheatFact.getAuthenticationModule();
+        this.authorMod = this.cheatFact.getAuthorisationModule();
+
+    }
+
+    //injectAuth() tests
+    @Test
+    public void injectAuthNullAuthen(){
+        assertThrows(IllegalArgumentException.class,() -> this.bsfi.injectAuth(null, this.authorMod));
+    }
+
+    @Test
+    public void injectAuthNullAuthor(){
+        assertThrows(IllegalArgumentException.class,() -> this.bsfi.injectAuth(this.authenMod, null));
+    }
+
+    //login() tests
+    @Test
+    public void loginBasic(){
+        this.bsfi.injectAuth(this.authenMod, this.authorMod);
+        assertTrue(this.bsfi.login("user", "password"));
+    }
+
+    @Test
+    public void loginBasicFalseName(){
+        this.bsfi.injectAuth(this.authenMod, this.authorMod);
+        assertFalse(this.bsfi.login("not-user", "password"));
+    }
+
+    @Test
+    public void loginBasicFalsePwd(){
+        this.bsfi.injectAuth(this.authenMod, this.authorMod);
+        assertFalse(this.bsfi.login("user", "not-password"));
+    }
+
+    @Test
+    public void loginNullName(){
+        this.bsfi.injectAuth(this.authenMod, this.authorMod);
+        assertThrows(IllegalArgumentException.class,() -> this.bsfi.login(null, "password"));
+    }
+
+    @Test
+    public void loginNullPwd(){
+        this.bsfi.injectAuth(this.authenMod, this.authorMod);
+        assertThrows(IllegalArgumentException.class,() -> this.bsfi.login("user", null));
+    }
+
+    @Test
+    public void loginNullBoth(){
+        this.bsfi.injectAuth(this.authenMod, this.authorMod);
+        assertThrows(IllegalArgumentException.class,() -> this.bsfi.login(null, null));
+    }
+
+    @Test
+    public void loginNoInject(){
+        assertThrows(IllegalStateException.class,() -> this.bsfi.login("user", "password"));
+    }
+
+    //logout() tests
+    @Test
+    public void logoutIllState1(){
+        assertThrows(IllegalStateException.class,() -> this.bsfi.logout());
+    }
+
+    @Test
+    public void logoutIllState2(){
+        this.bsfi.injectAuth(this.authenMod, this.authorMod);
+        assertThrows(IllegalStateException.class,() -> this.bsfi.logout());
+    }
+
+    @Test
+    public void logoutIllState3(){
+        this.bsfi.injectAuth(this.authenMod, this.authorMod);
+        this.bsfi.login("user", "password");
+        this.bsfi.logout();
+        assertThrows(IllegalStateException.class,() -> this.bsfi.logout());
+    }
+}
